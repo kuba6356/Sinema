8191c6a88b0b23b8a192c6bdb255e37c47972455
kuba6356 <kuba6356@gmail.com>
2025-04-29 13:15:33 +0200

    added user entity

--- src/main/java/com/Sinema/demo/users/User.java
+++ src/main/java/com/Sinema/demo/users/User.java
@@ -1,4 +1,79 @@
-package com.Sinema.demo.Users;
+package com.Sinema.demo.users;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.Objects;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+    @Email
+    private String email;
+    private String passwordHash;
+    private String role;
+
+    public void User(Long id, String email, String passwordHash, String role) {
+        this.id = id;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(passwordHash, user.passwordHash) && Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, passwordHash, role);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
